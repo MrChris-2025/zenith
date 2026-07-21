b42faca592b57fb7252a187211ec49ddbd99b05c
MrChris-2025 <7rings@protonmail.com>
2026-07-21 10:00:02 -0400

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,23 +1,17 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyB_ExrYOQfmaYQqqS9lof8klY9Y8Qbrbc8",
-    authDomain: "webpush721.firebaseapp.com",
-    projectId: "webpush721",
-    storageBucket: "webpush721.firebasestorage.app",
-    messagingSenderId: "638081013513",
-    appId: "1:638081013513:web:88960958d7e8f12be325d8",
-    measurementId: "G-ZE56P5MBX6"
-  };
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "webpush721.firebaseapp.com",
+  projectId: "webpush721",
+  storageBucket: "webpush721.firebasestorage.app",
+  messagingSenderId: "638081013513",
+  appId: "YOUR_APP_ID",
+  measurementId: "G-XXXXXXXXXX" // Put your real measurement ID here
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
