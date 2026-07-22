861f89aea4ef1c4ae783769a88d0b64d16e912fe
MrChris-2025 <7rings@protonmail.com>
2026-07-22 16:28:57 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,19 +5,19 @@ firebase.initializeApp({
   apiKey: "AIzaSyB_ExrYOQfmaYQqqS9lof8klY9Y8Qbrbc8",
   authDomain: "webpush721.firebaseapp.com",
   projectId: "webpush721",
+  storageBucket: "webpush721.firebasestorage.app",
   messagingSenderId: "638081013513",
   appId: "1:638081013513:web:88960958d7e8f12be325d8"
 });
 
 const messaging = firebase.messaging();
 
-// Display notification when the website is closed or in the background
 messaging.onBackgroundMessage((payload) => {
   console.log("Background message received: ", payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/firebase-logo.png' // You can replace this with your icon path later
+    icon: 'https://hcforever.nekoweb.org/mo2.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
