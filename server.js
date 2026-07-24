b64453fd6b565914d9a416de90d64c7e0119b32d
MrChris-2025 <7rings@protonmail.com>
2026-07-24 01:06:17 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve all static files (index.html, main.js, firebase.js)
+app.use(express.static(__dirname));
+
+// Send index.html for any web request
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
